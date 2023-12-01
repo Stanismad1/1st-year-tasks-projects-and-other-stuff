bb4abdc2ef52acc192241e5a80a701f1c2262357
Stanislav Gandzhov <152076566+Stanismad1@users.noreply.github.com>
2023-12-01 23:30:11 +0200

    Add files via upload

---
+++ up week 4.1 task.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+//zad1
+
+#include <iostream>
+using namespace std;
+
+
+double fahrenheitToCelsius(double degrees)
+{
+	double celsius = (degrees - 32) * 5 / 9;
+	return celsius;
+}
+int main ()
+ {
+	double fahrenheitDegrees;
+	cin >> fahrenheitDegrees;
+	double celsiusDegrees =
+		fahrenheitToCelsius(fahrenheitDegrees);
+	if (celsiusDegrees >= 37) cout << "You're ill";
+}
+
+
+
