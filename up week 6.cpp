00ceedbb3bf1d79bbeaa6e58a010aab4813a90df
Stanislav Gandzhov <152076566+Stanismad1@users.noreply.github.com>
2023-12-26 16:56:17 +0200

    Add files via upload

---
+++ up week 6.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+//zad0
+cout << "Do you want to continue? Y/N";
+cin >> c;
+if (c == 'y' || c == 'Y')
+cout << "Let's go!" << endl;
+
+	if (c >= 'a' && c <= 'z')
+		cout << "Small letter";
+
+c = 'A'; c += 3; cout << c; 
+#include <iostream>
+#include <cstring>
+
+//zad1
+
+using namespace std;
+
+
+char toLowerCase(char c)
+{
+    if (c >= 'A' && c <= 'Z')
+        return c - 'A' + 'a';
+    return c;
+}
+
+char toUpperCase(char c)
+{
+    if (c >= 'a' && c <= 'z')
+        return c - 'a' + 'A';
+    return c;
+}
+
+
+int findDigitalRoot(int number)
+{
+    int result = number % 9;
+    if (result == 0) result = 9; 
+    return result;
+}
